6eafe8a9f0eedf4a55db1b1ee25c86e4099f794a
Matiastrfo <trifiromatias@gmail.com>
2025-10-27 13:27:56 -0300

    commit: Compilador Java completo

--- compilar.bat
+++ compilar.bat
@@ -1,9 +1,25 @@
 @echo off
 echo Compilando proyecto...
-javac -cp "lib\antlr-4.13.0-complete.jar" -d target\classes src\main\java\compilador\**\*.java
+
+cd compilador-java
+
+:: Limpiar
+if exist "target" rmdir /s /q target
+mkdir target\classes
+
+:: Compilar
+echo Compilando archivos...
+dir /s /b *.java > archivos.txt
+javac -cp "lib\antlr-4.13.0-complete.jar" -d target\classes @archivos.txt
+
 if %errorlevel% equ 0 (
     echo ¡Compilación exitosa!
-    java -cp "target\classes;lib\antlr-4.13.0-complete.jar" compilador.Main
+    echo.
+    echo Ejecutando programa...
+    java -cp "target\classes;lib\antlr-4.13.0-complete.jar" main.java.compilador.Main
 ) else (
     echo Error en la compilación
 )
+
+if exist archivos.txt del archivos.txt
+pause
